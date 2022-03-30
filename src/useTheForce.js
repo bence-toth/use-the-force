bf948fe2599ec907f358b0e6678e62460614381b
Bence A. Tóth <tothab@gmail.com>
2022-03-30 14:25:21 +0200

    Implemented custom hook

---
+++ src/useTheForce.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useCallback } from "react";
+
+import fetchMemes, { getMemeData, hasThumbnail } from "./fetchMemes";
+
+const useTheForce = () => {
+  const [memes, setMemes] = useState([]);
+  const [isMemeLoading, setIsMemeLoading] = useState(true);
+
+  useEffect(() => {
+    setIsMemeLoading(true);
+    fetchMemes()
+      .then((response) => response.json())
+      .then((result) => {
+        const data = result.data.children.filter(hasThumbnail).map(getMemeData);
+        setMemes(data);
+        setIsMemeLoading(false);
+      });
+  }, []);
+
+  const [page, setPage] = useState(0);
+
+  const onLoadNextMeme = useCallback(() => {
+    if (page < memes.length - 1) {
+      setPage((page) => page + 1);
+    }
+  }, [page, memes]);
+
+  const onLoadPreviousMeme = useCallback(() => {
+    if (page > 0) {
+      setPage((page) => page - 1);
+    }
+  }, [page]);
+
+  return {
+    isMemeLoading,
+    meme: memes[page],
+    onLoadNextMeme,
+    onLoadPreviousMeme,
+  };
+};
+
+export default useTheForce;
