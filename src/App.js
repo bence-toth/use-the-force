77f6ccbfa557567b3144074a66d111ba76cb67c5
Bence A. Tóth <tothab@gmail.com>
2022-03-30 14:25:25 +0200

    Implemented app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import useTheForce from "./useTheForce";
+import "./App.css";
+
 const App = () => {
-  return <div></div>;
+  const { meme, isMemeLoading, onLoadNextMeme, onLoadPreviousMeme } =
+    useTheForce();
+
+  return (
+    <div class="app">
+      {isMemeLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="pageButtons">
+            <button onClick={onLoadPreviousMeme}>Previous</button>
+            <button onClick={onLoadNextMeme}>Next</button>
+          </div>
+          <div className="meme">
+            <a href={meme.url}>
+              <img src={meme.url} alt={meme.title} />
+            </a>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
